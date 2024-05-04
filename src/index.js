906e0c7dd9aea5b6b83784a920226b6045a53a84
Mario <caraimanmario1608@gmail.com>
2024-05-04 15:56:51 +0300

    added page title

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
+document.title = 'RoboFriends';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
